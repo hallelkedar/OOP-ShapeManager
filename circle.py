7b474027957269929366f99adcdbe881bd7e4197
Hallel <hallelkedar000@gmail.com>
2026-05-26 16:52:16 +0300

    add circle methods

---
+++ circle.py
@@ -0,0 +1,15 @@
+from shape import Shape
+
+class Circle(Shape):
+    
+    PI = 3.14159265359
+
+    def __init__(self, shape_id, shape_type, radius):
+        super().__init__(shape_id, shape_type)
+        self.radius = radius
+
+    def get_area(self):
+        return Circle.PI * (self.radius ** 2)
+    
+    def get_perimeter(self):
+        return 2 * Circle.PI * self.radius
