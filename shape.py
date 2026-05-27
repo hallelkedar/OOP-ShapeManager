e583c5d58a96e9698bcae62e80850221d3f86899
Hallel <hallelkedar000@gmail.com>
2026-05-27 12:32:52 +0300

    change from 'id' to 'shape_id' (to avoid bugs)

--- shape.py
+++ shape.py
@@ -1,6 +1,6 @@
 class Shape:
     def __init__(self, shape_id, shape_type):
-        self.id = shape_id
+        self.shape_id = shape_id
         self.shape_type = shape_type
 
     def get_area(self):
