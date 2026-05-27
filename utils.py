341ead850669e34dbbb80f27555493b04d2dd483
Hallel <hallelkedar000@gmail.com>
2026-05-27 11:57:51 +0300

    fix validation input func

--- utils.py
+++ utils.py
@@ -1,13 +1,18 @@
 class Utils:
     
     @staticmethod
-    def validation_input(input_type, req_msg, shapes=None):
+    def validation_input(input_type, req_msg, shapes=None, choice_range=None):
         while True:
             choice = input(req_msg)
             if input_type.lower() == 'number':
                 if choice.isdigit():
-                    if choice > 0:
-                        return choice
+                    choice_num = int(choice)
+                    if choice_range:
+                        if 0 < choice_num < choice_range+1:
+                            return choice
+                    
+                    elif choice > 0:
+                        return choice_num
                     
             elif input_type.lower() == 'shape':
                 if choice in shapes:
