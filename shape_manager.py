251b62c792466b7c78ac08e5ba6095c6b21b42dd
Hallel <hallelkedar000@gmail.com>
2026-05-27 11:57:01 +0300

    fix ShapeManager methods

--- shape_manager.py
+++ shape_manager.py
@@ -1,22 +1,37 @@
 from square import Square
 from rectangle import Rectangle
 from circle import Circle
-from utils import Utils
 import json
 
 class ShapeManager:
+    FILE_NAME = 'shapes.json'
+    SHAPES = {
+    'Square': Square,
+    'Rectangle': Rectangle,
+    'Circle': Circle
+    }
 
     def __init__(self):
         self.shapes = []
-        self.load_from_json()
+        self.load_from_json(self.FILE_NAME)
     
     def get_new_id(self):
-        return len(self.shapes) + 1
+        if not self.shapes:
+                return 1
+        
+        return max(shape.shape_id for shape in self.shapes) + 1
     
     def create_shape(self, shape: str, data: dict):
         data['shape_id'], data['shape_type'] = self.get_new_id(), shape
-        new_shape = shape(**data)
+        try:
+            new_shape = self.SHAPES[shape](**data)
+        except ValueError:
+            return False
+        
         self.shapes.append(new_shape)
+        self.save_to_json(self.FILE_NAME)
+
+        return True
         
     def get_all_shapes(self):
         output_list = []
@@ -26,31 +41,45 @@ class ShapeManager:
             output_list.append(shape_str)
         return output_list
     
-    def update_shape(self, shape_id: str, new_data: dict, file_name):
+    def update_shape(self, shape_id: int, new_data: dict):
         shape = self.get_shape_by_id(shape_id)
         if shape:
-            for value in new_data:    
-                shape.value = new_data[value]
-            self.shapes.append(shape)
-            self.save_to_json(file_name)
+            for k, v in new_data.items():    
+                setattr(shape, k, v)
+            self.save_to_json(self.FILE_NAME)
             return True
         
     def delete_shape(self, shape_id):
         shape = self.get_shape_by_id(shape_id)
         if shape:
             self.shapes.remove(shape)
+            self.save_to_json(self.FILE_NAME)
             return True
     
     def save_to_json(self, file_name):
+        
+        data = [shape.todict() for shape in self.shapes]
+
         with open(file_name, 'w') as f:
-            json.dump(self.shapes, f)
+            json.dump(data, f, indent=4)
 
     def load_from_json(self, file_name):
-        with open(file_name, 'r') as f:
-            self.shapes = json.load(f)
+        
+        try:
+            with open(file_name, 'r') as f:
+                data = json.load(f)
+            
+            self.shapes = []
+            for item in data:
+                shape_type = item.pop('shape_type')
+                shape = self.SHAPES[shape_type](**item)
+                self.shapes.append(shape)
+            
+        except FileNotFoundError:
+            self.shapes = []
 
-    def get_shape_by_id(self, id):
+    def get_shape_by_id(self, shape_id: int):
         for shape in self.shapes:
-            if shape['id'] == id:
+            if shape.shape_id == shape_id:
                 return shape
             
